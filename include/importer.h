f36f8cdbdc836e2c0b144e3ff6d7fc1dff762596
Saswat <87571040+SaswatJK@users.noreply.github.com>
2025-11-25 19:24:56 +0545

    Made an OBJ importer | Still unfinished because the same vertex can have different normals for different faces

--- include/importer.h
+++ include/importer.h
@@ -1,6 +1,19 @@
+#pragma once
 #include <utils.h>
 
-class Vertex;
+typedef struct{
+    uint32_t indices[3];
+}Mesh;
 
 class Model{
+public:
+    std::vector<Vertex> vertices;
+    std::vector<Mesh> meshes;
+    //We will check that the number of Meshes isn't going to be more than the maximum number of Triangles that can be made with 'N' Vertices:
+    //I calculated this using basic deduction through a complete graph: Maximum number of triangles in a complete graph is:
+    // N        n           |
+    // |E       |E (i - 2)  | E stands for summation.
+    // n = 2    i = 2       |
+    // Or simply put: NC3
+    void loadModel(const std::string &path);
 };
---
+++ source/importer.cpp
@@ -0,0 +1,65 @@
+#include "../include/importer.h"
+#include "utils.h"
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+void Model::loadModel(const std::string &path){
+    std::vector<Vec4> normals;
+    std::ifstream file(path);
+    std::string line;
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return;
+    }
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string identifier;
+        Vertex tempVertexData = {0.0};
+        Vec4 tempNormal;
+        if(iss >> identifier){
+            if (identifier == "v"){
+                float v1, v2, v3;
+                iss >> v1 >> v2 >> v3;
+                tempVertexData.position.x = v1;
+                tempVertexData.position.y = v2;
+                tempVertexData.position.z = v3;
+                tempVertexData.position.w = 1.0;
+                vertices.push_back(tempVertexData);
+
+            }
+            else if (identifier == "vn") {
+                float v1, v2, v3;
+                iss >> v1 >> v2 >> v3;
+                tempNormal.x = v1;
+                tempNormal.y = v2;
+                tempNormal.z = v3;
+                tempNormal.w = 0.0;
+                normals.push_back(tempNormal);
+            }
+            else if (identifier == "f"){
+                std::string group;
+                std::vector<uint32_t> groupNumbers;
+                Mesh tempMesh;
+                while (iss >> group) {
+                    std::istringstream groupStream(group);
+                    std::string numStr;
+                    while (std::getline(groupStream, numStr, '/')) {
+                        int num = std::stoi(numStr);
+                        groupNumbers.push_back(num-1); //Indices start from 0 in vectors.
+                    }
+                }
+                tempMesh.indices[0] = groupNumbers[0];
+                tempMesh.indices[1] = groupNumbers[3];
+                tempMesh.indices[2] = groupNumbers[6];
+                vertices[groupNumbers[0]].normal = normals[groupNumbers[2]];
+                vertices[groupNumbers[3]].normal = normals[groupNumbers[5]];
+                vertices[groupNumbers[6]].normal = normals[groupNumbers[8]];
+                meshes.push_back(tempMesh);
+            }
+        }
+    }
+    file.close();
+}
