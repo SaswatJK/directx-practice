8c866f7a370b5ba1dda1232b7c1137f01b87a297
Saswat <87571040+SaswatJK@users.noreply.github.com>
2026-01-24 22:31:04 +0545

    Integrated arenas to the obj importer

--- include/importer.h
+++ include/importer.h
@@ -6,11 +6,18 @@ typedef struct{
     uint32_t indices[3];
 }Face;
 
+typedef struct{
+    DataArray* modelVertices;
+    DataArray* modelIndices;
+    DataArray* modelMatrices;
+}ModelData;
+
 class Model{
 public:
-    std::vector<Vertex> vertices;
-    std::vector<Face> faces;
-    std::vector<unsigned int> vertexInfo; // Flag for each vertex, if it's duplicated or not.
+    Vertex* aVertices;
+    Face* aFaces;
+    u32 numFaces;
+    u32 numVertices;
     glm::mat4 modelMatrix;
     //We will check that the number of Meshes isn't going to be more than the maximum number of Triangles that can be made with 'N' Vertices:
     //I calculated this using basic deduction through a complete graph: Maximum number of triangles in a complete graph is:
@@ -18,11 +25,15 @@ public:
     // |E       |E (i - 2)  | E stands for summation.
     // n = 2    i = 2       |
     // Or simply put: NC3
-    void loadModel(std::string &modelPath, glm::vec3 worldPos, glm::vec3 worldRotate, glm::vec3 worldScale, uint32_t modelIndex, uint32_t indicesOffset);
+    void loadModel(std::string &modelPath, glm::vec3 worldPos, glm::vec3 worldRotate, glm::vec3 worldScale, uint32_t modelIndex, uint32_t indicesOffset, Arena* vertexArena, Arena* indicesArena);
 };
 
 class Models{
+    Arena vertexArena;
+    Arena indicesArena;
+    ModelData data;
 public:
     std::vector<Model> models;
-    void loadModels(const std::string &modelInfoPath);
+    ModelData* loadModels(const std::string &modelInfoPath, u32 minModelSizeInBytes);
+    ~Models();
 };
--- source/importer.cpp
+++ source/importer.cpp
@@ -1,13 +1,19 @@
 #include "../include/importer.h"
-#include "glm/detail/qualifier.hpp"
 #include "utils.h"
-#include <cstdint>
+#include <cstdio>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 
-void Model::loadModel(std::string &modelPath, glm::vec3 worldPos, glm::vec3 worldRotate, glm::vec3 worldScale, uint32_t modelIndex, uint32_t indicesOffset){
+void Model::loadModel(std::string &modelPath, glm::vec3 worldPos, glm::vec3 worldRotate, glm::vec3 worldScale, u32 modelIndex, u32 indicesOffset, Arena* vertexArena, Arena* indicesArena){
+    std::vector<unsigned int> vertexInfo; // Flag for each vertex, if it's duplicated or not.
+    GET_POINTER_IN_ARENA(*vertexArena, Vertex, aVertices);
+    Vertex* currentVertex = aVertices;
+    GET_POINTER_IN_ARENA(*indicesArena, Faces, aFaces);
+    Face* currentFace = aFaces;
+    u32 vertexNum = 0;
+    u32 faceNum = 0;
     std::vector<glm::vec4> normals;
     std::ifstream file(modelPath);
     std::string line;
@@ -26,13 +32,15 @@ void Model::loadModel(std::string &modelPath, glm::vec3 worldPos, glm::vec3 worl
         glm::vec4 tempNormal;
         if(iss >> identifier){
             if (identifier == "v"){
+                vertexNum++;
                 float v1, v2, v3;
                 iss >> v1 >> v2 >> v3;
                 tempVertexData.position.x = v1;
                 tempVertexData.position.y = v2;
                 tempVertexData.position.z = v3;
                 tempVertexData.position.w = 1.0;
-                vertices.push_back(tempVertexData);
+                *currentVertex = tempVertexData;
+                currentVertex++;
                 vertexInfo.push_back(tempInfo);
             }
             else if (identifier == "vn") {
@@ -45,8 +53,9 @@ void Model::loadModel(std::string &modelPath, glm::vec3 worldPos, glm::vec3 worl
                 normals.push_back(tempNormal);
             }
             else if (identifier == "f"){
+                faceNum++;
                 std::string group;
-                std::vector<uint32_t> groupNumbers;
+                std::vector<u32> groupNumbers;
                 Face tempFace;
                 while (iss >> group) {
                     std::istringstream groupStream(group);
@@ -58,44 +67,51 @@ void Model::loadModel(std::string &modelPath, glm::vec3 worldPos, glm::vec3 worl
                 }
                //Check to see if the normals have been repeated. If so, do some basic mathematics and logic and just repeat that vertex.
                 if(vertexInfo[groupNumbers[0]] == 3773){
-                    vertices[groupNumbers[0]].normal = normals[groupNumbers[2]];
+                    aVertices[groupNumbers[0]].normal = normals[groupNumbers[2]];
                     vertexInfo[groupNumbers[0]] = 79;
                 }
                 else{
-                    tempVertexData.position = vertices[groupNumbers[0]].position;
+                    tempVertexData.position = aVertices[groupNumbers[0]].position;
                     tempVertexData.normal = normals[groupNumbers[2]];
-                    groupNumbers[0] = vertices.size();
-                    vertices.push_back(tempVertexData);
+                    groupNumbers[0] = vertexNum;
+                    *currentVertex = tempVertexData;
+                    currentVertex++;
+                    vertexNum++;
                     vertexInfo.push_back(79);
                 }
                 if(vertexInfo[groupNumbers[3]] == 3773){
-                    vertices[groupNumbers[3]].normal = normals[groupNumbers[5]];
+                    aVertices[groupNumbers[3]].normal = normals[groupNumbers[5]];
                     vertexInfo[groupNumbers[3]] = 79;
                 }
                 else{
-                    tempVertexData.position = vertices[groupNumbers[3]].position;
+                    tempVertexData.position = aVertices[groupNumbers[3]].position;
                     tempVertexData.normal = normals[groupNumbers[5]];
-                    groupNumbers[3] = vertices.size();
-                    vertices.push_back(tempVertexData);
+                    groupNumbers[3] = vertexNum;
+                    *currentVertex = tempVertexData;
+                    currentVertex++;
+                    vertexNum++;
                     vertexInfo.push_back(79);
-
                 }
                 if(vertexInfo[groupNumbers[6]] == 3773){
-                    vertices[groupNumbers[6]].normal = normals[groupNumbers[8]];
+                    aVertices[groupNumbers[6]].normal = normals[groupNumbers[8]];
                     vertexInfo[groupNumbers[6]] = 79;
                 }
                 else{
-                    tempVertexData.position = vertices[groupNumbers[6]].position;
+                    tempVertexData.position = aVertices[groupNumbers[6]].position;
                     tempVertexData.normal = normals[groupNumbers[8]];
-                    groupNumbers[6] = vertices.size();
-                    vertices.push_back(tempVertexData);
+                    groupNumbers[6] = vertexNum;
+                    *currentVertex = tempVertexData;
+                    currentVertex++;
+                    vertexNum++;
                     vertexInfo.push_back(79);
-
                 }
                 tempFace.indices[0] = groupNumbers[0] + indicesOffset;
                 tempFace.indices[1] = groupNumbers[3] + indicesOffset;
                 tempFace.indices[2] = groupNumbers[6] + indicesOffset;
-                faces.push_back(tempFace);
+                *currentFace= tempFace;
+                if (indicesOffset == 0)
+                    printf("%d, %d, %d \n", tempFace.indices[0], tempFace.indices[1], tempFace.indices[2]);
+                currentFace++;
             }
         }
     }
@@ -103,9 +119,19 @@ void Model::loadModel(std::string &modelPath, glm::vec3 worldPos, glm::vec3 worl
     glm::mat4 Scale = glm::scale(glm::mat4(1.0f), worldScale);
     glm::mat4 Trans = glm::translate(glm::mat4(1.0f), worldPos);
     modelMatrix = Trans * Scale;
+    PUSH_POINTER_IN_ARENA(*vertexArena, Vertex, vertexNum);
+    PUSH_POINTER_IN_ARENA(*indicesArena, Face, faceNum);
+    numFaces = faceNum;
+    numVertices = vertexNum;
+    printf("The index is: %d\n", aFaces->indices[0]);
+    //printf("The number of indices is: %d \n", numFaces * 3);
 }
 
-void Models::loadModels(const std::string &modelInfoPath){
+ModelData* Models::loadModels(const std::string &modelInfoPath, u32 minModelSizeInBytes){
+    vertexArena.reserveArena(minModelSizeInBytes);
+    vertexArena.commitArena(minModelSizeInBytes);
+    indicesArena.reserveArena(minModelSizeInBytes);
+    indicesArena.commitArena(minModelSizeInBytes);
     std::vector<std::string> actualPath;
     std::ifstream filePath(modelInfoPath);
     std::vector<glm::vec3> modelWorldPos;
@@ -113,7 +139,7 @@ void Models::loadModels(const std::string &modelInfoPath){
     std::vector<glm::vec3> modelWorldScl;
     if (!filePath.is_open()) {
         std::cerr << "Error opening modelInfo file!" << std::endl;
-        return;
+        return NULL;
     }
     std::string fileLine;
     while (std::getline(filePath, fileLine)){
@@ -155,9 +181,66 @@ void Models::loadModels(const std::string &modelInfoPath){
         }
     }
     models.resize(actualPath.size());
-    uint32_t modelIndicesOffset = 0;
-    for(size_t i = 0; i < actualPath.size(); i++){
-        models[i].loadModel(actualPath[i], modelWorldPos[i], modelWorldRot[i], modelWorldScl[i], i, modelIndicesOffset);
-        modelIndicesOffset += (models[i].faces.size() * 3);
+    u32 modelNum = actualPath.size();
+    u32 modelIndicesOffset = 0;
+    for(size_t i = 0; i < modelNum; i++){
+        models[i].loadModel(actualPath[i], modelWorldPos[i], modelWorldRot[i], modelWorldScl[i], i, modelIndicesOffset, &vertexArena, &indicesArena);
+        modelIndicesOffset += (models[i].numFaces * 3);
     }
+    printf("The index in 'models' is: %d\n", models[0].aFaces->indices[0]);
+    ARENA_ERROR error;
+    DataArray* tempPtr;
+    error = GET_POINTER_IN_ARENA(vertexArena, DataArray, tempPtr);
+    if (error != ARENA_OK) std::cerr<<"Arena: "<<error;
+    data.modelVertices = tempPtr;
+    data.modelIndices = tempPtr + 1;
+    data.modelMatrices = tempPtr + 2;
+    error = PUSH_POINTER_IN_ARENA(vertexArena, DataArray, 3);
+    if (error != ARENA_OK) std::cerr<<"Arena: "<<error;
+    VertexSizePair* vertexVSP;
+    VertexSizePair* currentPointer;
+    error = GET_POINTER_IN_ARENA(vertexArena, VertexSizePair, vertexVSP);
+    if (error != ARENA_OK) std::cerr<<"Arena: "<<error;
+    currentPointer = vertexVSP;
+    for(size_t i = 0; i < modelNum; i++){
+        currentPointer->size = models[i].numVertices * sizeof(Vertex);
+        currentPointer->data = models[i].aVertices;
+        currentPointer++;
+    }
+    error = PUSH_POINTER_IN_ARENA(vertexArena, VertexSizePair, modelNum);
+    if (error != ARENA_OK) std::cerr<<"Arena: "<<error;
+    PtrSizePair* indicesPSP;
+    PtrSizePair* currentPtr;
+    error = GET_POINTER_IN_ARENA(vertexArena, PtrSizePair, indicesPSP);
+    if (error != ARENA_OK) std::cerr<<"Arena: "<<error;
+    currentPtr = indicesPSP;
+    for(size_t i = 0; i < modelNum; i++){
+        currentPtr->size = models[i].numFaces * sizeof(Face);
+        //printf("The num of indices after dataArray are: %d \n", currentPtr->size / sizeof(u32));
+        currentPtr->data = models[i].aFaces;
+        printf("The index is: %d \n", models[i].aFaces->indices[0]);
+        currentPtr++;
+    }
+    PtrSizePair* matPSP = currentPtr;
+    error = PUSH_POINTER_IN_ARENA(vertexArena, PtrSizePair, modelNum);
+    if (error != ARENA_OK) std::cerr<<"Arena: "<<error;
+    for(size_t i = 0; i < modelNum; i++){
+        currentPtr->data = &models[i].modelMatrix;
+        currentPtr->size = sizeof(glm::mat4);
+        currentPtr++;
+    }
+    error = PUSH_POINTER_IN_ARENA(vertexArena, PtrSizePair, modelNum);
+    if (error != ARENA_OK) std::cerr<<"Arena: "<<error;
+    data.modelVertices->VSPArray.arr = vertexVSP;
+    data.modelVertices->VSPArray.count = modelNum;
+    data.modelMatrices->PSPArray.arr = matPSP;
+    data.modelMatrices->PSPArray.count = modelNum;
+    data.modelIndices->PSPArray.count = modelNum;
+    data.modelIndices->PSPArray.arr = indicesPSP;
+    return &data;
+}
+
+Models::~Models(){
+    vertexArena.removeArena();
+    indicesArena.removeArena();
 }
